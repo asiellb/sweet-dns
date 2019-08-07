ac71afcd85f90f05468d694c2a4b02c69ce95f58
Asiel Lara <asiel.lb@stic.cl>
2019-08-07 15:03:42 -0400

    fix install script bug on non installed dnsmasq, add message variable to end

--- install.sh
+++ install.sh
@@ -13,31 +13,30 @@ else
     if [[ $? -eq 1 ]]; then
         echo -e "\033[1mInfo >>>\033[0m Running dnsmasq installation..."
         HOMEBREW_NO_AUTO_UPDATE=1 brew install dnsmasq
+    fi
+    which sweet-dns > /dev/null
+    if [[ $? -eq 1 ]]; then
+        echo -e "\033[1mInfo >>>\033[0m Running sweet-dns installation..."
+        curl -L https://git.io/fj9Jz -o $dst -s > /dev/null
+        chmod +x $dst
+        message="\033[1mInfo >>>\033[0m Installation successfull! \n
+        For help use \033[1msweet-dns -h\033[0m \n "
     else
-        which sweet-dns > /dev/null
-        if [[ $? -eq 1 ]]; then
-            echo -e "\033[1mInfo >>>\033[0m Running sweet-dns installation..."
-            curl -L https://git.io/fj9Jz -o $dst -s > /dev/null
-            chmod +x $dst
-            sweet-dns -c
-            echo -e "\033[1mInfo >>>\033[0m Installation successfull! \n
+        curl -L https://git.io/fj9Jz -o $tmpdst -s > /dev/null
+        if cmp $tmpdst $dst > /dev/null; then
+            message="\033[1mInfo >>>\033[0m sweet-dns already installed on your system and up to date! \n
             For help use \033[1msweet-dns -h\033[0m \n "
         else
-            curl -L https://git.io/fj9Jz -o $tmpdst -s > /dev/null
-            if cmp $tmpdst $dst > /dev/null; then
-                echo -e "\033[1mInfo >>>\033[0m sweet-dns already installed on your system and up to date! \n
+            echo -e "\033[1mInfo >>>\033[0m Updating sweet-dns installation..."
+            cp -rf $tmpdst $dst
+            message="\033[1mInfo >>>\033[0m Update successfull! \n
             For help use \033[1msweet-dns -h\033[0m \n "
-            else
-                echo -e "\033[1mInfo >>>\033[0m Updating sweet-dns installation..."
-                cp $tmpdst $dst
-                sweet-dns -c
-                echo -e "\033[1mInfo >>>\033[0m Update successfull! \n
-            For help use \033[1msweet-dns -h\033[0m \n "
-            fi
-            rm $tmpdst
         fi
+        rm $tmpdst
     fi
     if [[ ! -f /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist ]]; then sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons; fi
     if ! sudo launchctl list | grep dnsmasq > /dev/null; then sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist; fi
+    sweet-dns -c
+    echo -e $message
     exit 0
 fi
