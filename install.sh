a2bc5c28a864f28e688e14715ea2ad9e7e17e04a
Asiel Lara <asiel.lb@stic.cl>
2019-09-26 14:43:18 -0300

    update resolv file dir, add plist to watch network, add watching network script, moving daemon configuration to main script, update install script, and add netwrokcange monitoring script

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-#Definitions
-dst=/usr/local/bin/sweet-dns
+#
+# sweet-dns install utility
+#
+# Definitions
+optdir=/usr/local/opt/sweet-dns
+dstdir=/usr/local/bin/sweet-dns
+tmpzip=/private/tmp/sweet-dns-master.zip
 tmpdst=/private/tmp/sweet-dns
+downver=$(grep "Version:" $tmpdst | awk '{print $3}')
+usedver=$(grep "Version:" $dstdir | awk '{print $3}')
 #Main
 echo -e "\033[1mInfo >>>\033[0m Running sweet-dns installation..."
 which brew > /dev/null
@@ -17,25 +24,37 @@ else
     which sweet-dns > /dev/null
     if [[ $? -eq 1 ]]; then
         echo -e "\033[1mInfo >>>\033[0m Running sweet-dns installation..."
-        curl -L https://git.io/fj9Jz -o $dst -s > /dev/null
-        chmod +x $dst
+        wget -O $tmpzip https://github.com/asiellb/sweet-dns/archive/master.zip
+        unzip -joq $tmpzip 'sweet-dns-master/*' -d sweet-dns
+        ln -s $optdir/sweet-dns $dstdir
+        chmod +x $dstdir
         message="\033[1mInfo >>>\033[0m Installation successfull! \n
         For help use \033[1msweet-dns -h\033[0m \n "
     else
         curl -L https://git.io/fj9Jz -o $tmpdst -s > /dev/null
-        if cmp $tmpdst $dst > /dev/null; then
+        if cmp $tmpdst $dstdir > /dev/null; then
             message="\033[1mInfo >>>\033[0m sweet-dns already installed on your system and up to date! \n
             For help use \033[1msweet-dns -h\033[0m \n "
         else
             echo -e "\033[1mInfo >>>\033[0m Updating sweet-dns installation..."
-            cp -rf $tmpdst $dst
-            message="\033[1mInfo >>>\033[0m Update successfull! \n
-            For help use \033[1msweet-dns -h\033[0m \n "
+            if [[ $downver == $usedver ]]; then
+                cp -rf $tmpdst $optdir/sweet-dns
+                chmod +x $dstdir
+                message="\033[1mInfo >>>\033[0m Partial update successfull! \n
+                For help use \033[1msweet-dns -h\033[0m \n "
+            else
+                wget -O $tmpzip https://github.com/asiellb/sweet-dns/archive/master.zip
+                unzip -joq $tmpzip 'sweet-dns-master/*' -d sweet-dns
+                ln -s $optdir/sweet-dns $dstdir
+                chmod +x $dstdir
+                message="\033[1mInfo >>>\033[0m Full update successfull! \n
+                For help use \033[1msweet-dns -h\033[0m \n "
+            fi
+
         fi
         rm $tmpdst
+        rm $tmpzip
     fi
-    if [[ ! -f /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist ]]; then sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons; fi
-    if ! sudo launchctl list | grep dnsmasq > /dev/null; then sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist; fi
     sweet-dns -c
     echo -e $message
     exit 0
---
+++ share/bin/networkchange.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# sweet-dns network changes watch utility script
+#
+# Definitions
+prefix=$(brew --prefix)
+dnsmasqdir=$prefix/etc/dnsmasq/dnsmasq.d
+iface=$(route get example.com | grep interface | awk '{print $2}')
+netservice=$(networksetup -listnetworkserviceorder | grep $iface | sed s/[,]/""/g | awk '{print $3}')
+#Main
+cat /private/var/run/resolv.conf > $dnsmasqdir/connection_resolv.conf
+networksetup -setdnsservers $netservice 127.0.0.1
+
+if [[ ! $? -eq 0 ]]; then
+    exit 1
+else
+    exit 0
+fi
